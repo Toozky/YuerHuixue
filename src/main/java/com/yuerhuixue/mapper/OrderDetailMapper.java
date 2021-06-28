6dd1d8c8fe3e54f6591c476927111cbe98c979d2
pocoalegre <pocoalegre@163.com>
2021-06-28 17:31:32 +0800

    订单

--- src/main/java/com/yuerhuixue/mapper/OrderDetailMapper.java
+++ src/main/java/com/yuerhuixue/mapper/OrderDetailMapper.java
@@ -2,11 +2,14 @@ package com.yuerhuixue.mapper;
 
 import com.yuerhuixue.pojo.OrderDetail;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public interface OrderDetailMapper {
+
     //根据ordermajor_id查订单
     List<OrderDetail> orderDetailFindByOrderMajorId(@Param("id")Integer id) throws SQLException;
 
